8e6d941f802d7f1a0bbddc5aaed1b3aacd238580
Mozhgan <mojganshah@gmail.com>
2024-09-18 14:51:22 +0330

    counting of courses,minutes,users

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 const $ = document;
 const landingTitle = $.querySelector(".landing__title");
+const landingCoursesCount = $.querySelector("#courses-count");
+const landingMinutesCount = $.querySelector("#courses-minutes");
+const usersCount = $.querySelector("#users-count");
 
 window.addEventListener("load", () => {
   let landingText = "ما به هر قیمتی دوره آموزشی تولید نمیکنیم!";
   let typingIndex = 0;
 
   typeWriter(landingText, typingIndex);
+  counting(40, landingCoursesCount);
+  counting(31_320, landingMinutesCount);
+  counting(31_700, usersCount);
 });
 
 function typeWriter(text, index) {
@@ -16,4 +22,17 @@ function typeWriter(text, index) {
   setTimeout(() => {
     typeWriter(text, index);
   }, 100);
+}
+
+function counting(max, el) {
+  let counter = 0;
+
+  const interval = setInterval(() => {
+    if (counter === max) {
+      clearInterval(interval);
+    }
+
+    el.innerHTML = counter;
+    counter++;
+  }, 30);
 }
