69b3dfb1671a3b77ad299b4a3b35dd90beac365f
Mozhgan <mojganshah@gmail.com>
2024-10-08 21:56:57 +0330

    filtering courses and rendering in dom through js

--- frontend/js/account/funcs/edit.js
+++ frontend/js/account/funcs/edit.js
@@ -7,7 +7,7 @@ const emailInputElem = document.querySelector("#email");
 const passwordInputElem = document.querySelector("#password");
 
 const showUserInfoInEditPage = async () => {
-  const res = await fetch(`http://localhost:4000/v1/auth/me`, {
+  const res = await fetch(`http://127.0.0.1:4000/v1/auth/me`, {
     headers: {
       Authorization: `Bearer ${getToken()}`,
     },
